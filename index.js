0e7b389a56d5c574c7b1d81750f84e4cded26634
Milan Brkic <milan.brkic1998@gmail.com>
2024-04-08 22:07:46 +0200

    add unsecured password field

--- index.js
+++ index.js
@@ -1,17 +1,36 @@
 const express = require("express");
 const { execSync } = require("child_process"); // Require execSync from child_process module
 const app = express();
+require("dotenv").config();
 const port = 1337;
 
 // Serve static files from the "public" directory
 app.use(express.static("public"));
-
+app.use(express.json());
 app.use(logMiddleware);
 
 app.post("/power-on", (req, res) => {
+  const password = req.body?.password;
+
+  if (!password) {
+    res.status(404).send("Password not entered");
+    console.error("Password not entered");
+    return;
+  }
+
+  if (!isPasswordCorrect(password)) {
+    res.status(404).send("Password is incorrect");
+    console.error("Password incorrect");
+    return;
+  }
+
   powerOnPc(60);
   res.send("PC powered on");
 });
+
+function isPasswordCorrect(password) {
+  return password === process.env.PASSWORD;
+}
 
 app.get("/health", (req, res) => {
   res.send("Cao");
